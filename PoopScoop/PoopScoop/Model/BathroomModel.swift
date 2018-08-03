13e8bcb265e6c679f00dd09a5e49a4c5edebd7ea
Chin-Yuan Hsu <chinyuan@usc.edu>
2018-08-03 13:43:30 -0400

    create BathroomModel

---
+++ PoopScoop/PoopScoop/Model/BathroomModel.swift
@@ -0,0 +1,13 @@
+//
+//  BathroomModel.swift
+//  PoopScoop
+//
+//  Created by jeff hsu on 8/3/18.
+//  Copyright © 2018 Jeffrey Hsu. All rights reserved.
+//
+
+import UIKit
+
+class BathroomModel: NSObject {
+
+}
