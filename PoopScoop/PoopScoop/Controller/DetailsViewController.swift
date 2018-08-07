30ac5e0261bac23c0fdba467ea398c8ffa733b96
Chin-Yuan Hsu <chinyuan@usc.edu>
2018-08-07 17:43:49 +0800

    separate setupViews to setupSubviews and makeConstraints

--- PoopScoop/PoopScoop/Controller/DetailsViewController.swift
+++ PoopScoop/PoopScoop/Controller/DetailsViewController.swift
@@ -12,22 +12,53 @@ class DetailsViewController: UIViewController {
     
     var bathroomModel: BathroomModel?
     
+    let scrollView = UIScrollView()
+    let containerView = UIView()
+    let imageView = UIImageView()
+    let bathroomNameLabel = UILabel()
+    let addressLabel = UILabel()
+    let distanceLabel = UILabel()
+    let timeEstimateLabel = UILabel()
+    let ratingLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
-        setupViews()
+        
+        setupSubviews()
+        makeConstraints()
     }
     
-    private func setupViews() {
-        self.view.addSubview(scrollView)
+    private func setupSubviews() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        
+        view.addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = bathroomModel?.previewImage
+        
+        containerView.addSubview(bathroomNameLabel)
+        bathroomNameLabel.text = bathroomModel?.name
+        bathroomNameLabel.adjustsFontSizeToFitWidth = true
+        bathroomNameLabel.textColor = UIColor.black
+        bathroomNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func makeConstraints() {
         scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         scrollView.contentSize.height = 800
         
-        self.view.addSubview(containerView)
         containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
         containerView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
@@ -37,74 +68,12 @@ class DetailsViewController: UIViewController {
         imageView.leftAnchor.constraint(equalTo: containerView.leftAnchor).isActive = true
         imageView.rightAnchor.constraint(equalTo: containerView.rightAnchor).isActive = true
         imageView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.4).isActive = true
         
         containerView.addSubview(bathroomNameLabel)
         bathroomNameLabel.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 15).isActive = true
         bathroomNameLabel.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -15).isActive = true
         bathroomNameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
         bathroomNameLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        
-        containerView.addSubview(bathroomDescriptionLabel)
-        bathroomDescriptionLabel.leftAnchor.constraint(equalTo: bathroomNameLabel.leftAnchor).isActive = true
-        bathroomDescriptionLabel.rightAnchor.constraint(equalTo: bathroomNameLabel.rightAnchor).isActive = true
-        bathroomDescriptionLabel.topAnchor.constraint(equalTo: bathroomNameLabel.bottomAnchor, constant: 10).isActive = true
-        bathroomDescriptionLabel.text = "SWAGGY SWAGGY DADDY"
-        bathroomDescriptionLabel.sizeToFit()
-        
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    let scrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.showsHorizontalScrollIndicator = false
-        scrollView.showsVerticalScrollIndicator = false
-        
-        return scrollView
-    }()
-    
-    let containerView: UIView = {
-       let containerView = UIView()
-        
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        return containerView
-    }()
-    
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        //TO-DO: PUT IN IMAGE imageView.image = #imageLiteral(resourceName: "bathroom1")
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        
-        return imageView
-    }()
-    
-    let bathroomNameLabel: UILabel = {
-        let bathroomNameLabel = UILabel()
-        
-        bathroomNameLabel.text = "Name"
-        bathroomNameLabel.font = UIFont.systemFont(ofSize: 28)
-        bathroomNameLabel.textColor = UIColor.black
-        bathroomNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        return bathroomNameLabel
-    }()
-    
-    let bathroomDescriptionLabel: UILabel = {
-        let label = UILabel()
-        
-        label.text = "Description"
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 20)
-        label.textColor = UIColor.black
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        return label
-    }()
 }
